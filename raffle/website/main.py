4c6c800de673d73d1c3c4ac8d601570a378f9b0d
QUADRI BASIT AYOMIDE <115375709+Ayomide248@users.noreply.github.com>
2023-10-14 10:09:59 +0100

    first commit

---
+++ raffle/website/main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'huuyufyuhfuygyfg hufuuouuyfgyuguiu'  
+# Store the admin's winning number
+admin_winning_number = 0
+
+@app.route('/raffle', methods=['GET', 'POST'])
+def raffle():
+    global admin_winning_number  
+    
+    if request.method == 'POST':
+        selected_number = int(request.json['number'])  
+
+        if selected_number == admin_winning_number:
+            return redirect(url_for('winning_page'))
+        else:
+            return redirect(url_for('sorry_page'))
+
+    return render_template('raffle.html', winning_number=admin_winning_number)
+
+@app.route('/spin', methods=['GET'])
+def spin():
+ 
+    return render_template('index.html')
+
+@app.route('/winning_page')
+def winning_page():
+    return render_template('winning.html')  # Adjust this route and template as needed
+
+@app.route('/sorry_page')
+def sorry_page():
+    return render_template('sorry.html') 
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    global admin_winning_number  # Access the global admin number
+
+    if request.method == 'POST':
+        admin_winning_number = int(request.form['winning_number'])
+        redirect(url_for("admin"))
+
+    return render_template('admin.html', winning_number=admin_winning_number)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, threaded=True)
